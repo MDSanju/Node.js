f66e6bd3c12a5e9420ac29ceef69c5a5b4f1603e
MD Sanju <mdsanju387@gmail.com>
2022-07-29 17:27:20 +0600

    external modules in Node.js

---
+++ external-modules/index.js
@@ -0,0 +1,5 @@
+const getRandomFruitsName = require("random-fruits-name");
+const randomMovieNames = require("random-movie-names");
+
+console.log(getRandomFruitsName("en"));
+console.log(randomMovieNames());
